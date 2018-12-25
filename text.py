c957f45863b55e4756ae48ff4070a68230e5c77e
Nefendi <michakacki@gmail.com>
2018-12-25 13:16:34 +0100

    Modificate further towards improving CLI

--- text.py
+++ text.py
@@ -74,6 +74,84 @@ def create_lstm_layer_with_backward_pass(num_of_fw_lstms, num_of_bw_lstms, lstm_
     return feedforward_outputs
 
 
+def load_and_parse_data(filename, seq_length, train_test_dataset_split_factor):
+
+    with open(filename, 'r') as f:
+        data = f.read()
+
+    # Create a TextBlob object from the input data
+    data = TextBlob(data)
+    # Get a part-of-speech tag describing each word and strip the text of all additional characters like commas, dots, hyphens, etc.
+    words_pos_tags = data.tags
+    # Obtain words from tuples of (word, pos_tag) in order to encode them
+    words = [word for word, tag in words_pos_tags]
+    # Remove duplicates
+    unique_words = list(set(words))
+
+    words_pos_tags_indexed = []
+
+    # Turn POS tags into numbers in the following way:
+    # - noun --> 0
+    # - verb --> 1
+    # - adjective --> 2
+    # - adverb --> 3
+    # - other --> 4
+    for word, tag in words_pos_tags:
+        if (tag[0] == 'N'):
+            words_pos_tags_indexed.append((word, 0))
+        elif (tag[0] == 'V'):
+            words_pos_tags_indexed.append((word, 1))
+        elif (tag[0] == 'J'):
+            words_pos_tags_indexed.append((word, 2))
+        elif (tag[0] == 'R'):
+            words_pos_tags_indexed.append((word, 3))
+        else:
+            words_pos_tags_indexed.append((word, 4))
+
+    # A dictionary to turn an index into a word
+    ix_to_word = {ix: word for ix, word in enumerate(unique_words)}
+    # A dictionary to turn a word into an index
+    word_to_ix = {word: ix for ix, word in enumerate(unique_words)}
+    # A dictionary to turn an index into a part of speech
+    ix_to_tag = {0: 'NOUN', 1: 'VERB', 2: 'ADJECTIVE', 3: 'ADVERB', 4: 'OTHER'}
+
+    # Number of unique words
+    vocabulary_size = len(unique_words)
+    # Number of labels
+    num_of_labels = len(ix_to_tag)
+    # Number of sequences that can be created from the input data
+    num_of_sequences = len(words) // seq_length
+    # How much of the data is to be used for training
+    train_data_num = int(train_test_dataset_split_factor * num_of_sequences)
+    test_data_num = num_of_sequences - train_data_num
+
+    # Data preparation, one-hot encoded words and labels
+    X = np.zeros((num_of_sequences, seq_length, vocabulary_size))
+    y = np.zeros((num_of_sequences, seq_length, num_of_labels))
+    for i in range(0, num_of_sequences):
+        X_sequence = words_pos_tags_indexed[i *
+                                            seq_length:(i+1)*seq_length]
+        X_sequence_ix = [word_to_ix[word] for word, tag in X_sequence]
+        input_sequence = np.zeros((seq_length, vocabulary_size))
+        for j in range(seq_length):
+            input_sequence[j][X_sequence_ix[j]] = 1.
+        X[i] = input_sequence
+
+        target_sequence = np.zeros((seq_length, num_of_labels))
+        for j in range(seq_length):
+            target_sequence[j][X_sequence[j][1]] = 1.
+        y[i] = target_sequence
+
+    # Divide the dataset into training and testing parts
+    trainX = X[:train_data_num]
+    trainY = y[:train_data_num]
+
+    testX = X[train_data_num:]
+    testY = y[train_data_num:]
+
+    return vocabulary_size, num_of_labels, ix_to_word, word_to_ix, ix_to_tag, trainX, trainY, testX, testY, train_data_num, test_data_num, words
+
+
 def build_text_annotation_model(is_onedirectional, lstm_size, fw_lstm_fst, bw_lstm_fst, fw_lstm_snd, bw_lstm_snd, feedforward_units_fst, feedforward_units_snd, data):
     if is_onedirectional:
         # Create LSTM layers with forward pass only
@@ -93,7 +171,7 @@ def build_text_annotation_model(is_onedirectional, lstm_size, fw_lstm_fst, bw_ls
     return logits
 
 
-def train_text_annotation_model(logits, targets, epochs):
+def train_text_annotation_model(logits, targets, trainX, trainY, testX, testY, train_data_num, test_data_num, epochs):
     # Pass the output from the network to the softmax activation function and compute the cross entropy after that
     loss = tf.nn.softmax_cross_entropy_with_logits_v2(
         labels=targets, logits=logits)
@@ -126,50 +204,57 @@ def train_text_annotation_model(logits, targets, epochs):
         trainY_shuffled = [trainY[a] for a in shuffle_indices]
 
         # Feed one SEQUENCE_LENGTH at a time to the network. Since RNN demands batch size, as the first dimension, the list of inputs must be passed to it. Hence, trainX_shuffled[i] is enclosed in square brackets to make a one-item list from it. Feed_dict is an argument used for passing data to previously defined placeholders. Train_step is computed
-        for i in range(TRAIN_DATA_NUM):
+        for i in range(train_data_num):
             sess.run(train_step,
                      feed_dict={input_data: [trainX_shuffled[i]], targets: trainY_shuffled[i]})
 
         # Compute the mean accuracy of the network after each epoch over the entire test dataset
         total_test_acc = 0
 
-        for k in range(TEST_DATA_NUM):
+        for k in range(test_data_num):
             test_acc = sess.run(accuracy,
                                 feed_dict={input_data: [testX[k]], targets: testY[k]})
             total_test_acc += test_acc
 
         print(
-            f"Testing accuracy at epoch {j+1}: {(total_test_acc / TEST_DATA_NUM):.2f}")
+            f"Testing accuracy at epoch {j+1}: {(total_test_acc / test_data_num):.2f}")
 
         return sess
 
 
-def test_text_annotation_model(logits, targets, sess):
-
-    # Output 1 if a character was correctly predicted and 0 if it was not
-    correct_prediction = tf.equal(tf.argmax(targets, 1), tf.argmax(logits, 1))
-
-    # Compute the mean to get the accuracy
-    accuracy = (tf.reduce_mean(tf.cast(correct_prediction, tf.float32))) * 100
-
-    print("\nStarting the testing phase!\n")
-
-    total_test_acc = 0
-
-    for k in range(TEST_DATA_NUM):
-        test_acc = sess.run(accuracy,
-                            feed_dict={input_data: [testX[k]], targets: testY[k]})
-        total_test_acc += test_acc
-
-    print(f"Testing accuracy: {(total_test_acc / TEST_DATA_NUM):.2f}")
-
-
-def predict_text_annotation_model(logits, output_file, sess):
+def predict_text_annotation_model(logits, ix_to_word, word_to_ix, seq_length, num_of_labels, vocabulary_size, original_dataset, input_file, output_file, sess):
     # Create an operation for predicting after training - only softmax, no cross entropy, we do not want to calculate loss
     prediction = tf.nn.softmax(logits, name='pred')
 
+    print("\nStarted loading and parsing the data...")
+
+    with open(input_file, 'r') as f:
+        data = f.read()
+
+    # Create a TextBlob object from the input data
+    data = TextBlob(data)
+    words = data.words
+
+    # Remove words that were not present in the training and test dataset
+    words = [word for word in words if word in original_dataset]
+
+    # Number of sequences that can be created from the input data
+    num_of_sequences = len(words) // seq_length
+
+    # Data preparation, one-hot encoded words and labels
+    X = np.zeros((num_of_sequences, seq_length, vocabulary_size))
+    for i in range(0, num_of_sequences):
+        X_sequence = words[i * seq_length: (i+1) * seq_length]
+        X_sequence_ix = [word_to_ix[word] for word in X_sequence]
+        input_sequence = np.zeros((seq_length, vocabulary_size))
+        for j in range(seq_length):
+            input_sequence[j][X_sequence_ix[j]] = 1.
+        X[i] = input_sequence
+
+    print("\nFinished loading and parsing the data!")
+
     print(
-        f"\nPrinting words and their predicted and actual labels to the '{output_file}' file...")
+        f"\nPrinting words and their predicted labels to the '{output_file}' file...")
 
    # After the training dump the decoded outputs to a file. The format is: <WORD> <PREDICTED_LABEL> <ACTUAL_LABEL>
 
@@ -177,86 +262,28 @@ def predict_text_annotation_model(logits, output_file, sess):
 
         padding = 25
 
-        print("{0: <{3}}{1: <{3}}{2: <{3}}\n".format(
-            "WORD", "PREDICTED_LABEL", "ACTUAL_LABEL", padding), file=g)
+        print("{0: <{2}}{1: <{2}}\n".format(
+            "WORD", "PREDICTED_LABEL", padding), file=g)
 
-        for k in range(TEST_DATA_NUM):
-            test_input = testX[k]
-            test_labels = testY[k]
+        for k in range(num_of_sequences):
+            prediction_input = X[k]
 
-            out = sess.run(prediction, feed_dict={input_data: [test_input]})
+            out = sess.run(prediction, feed_dict={
+                           input_data: [prediction_input]})
 
-            test_input_decoded = [
-                ix_to_word[np.argmax(code)] for code in test_input]
+            prediction_input_decoded = [
+                ix_to_word[np.argmax(code)] for code in prediction_input]
 
-            test_output_decoded = [
+            prediction_output_decoded = [
                 ix_to_tag[np.argmax(fragment)] for fragment in out]
-
-            test_labels_decoded = [
-                ix_to_tag[np.argmax(label)] for label in test_labels]
 
             for l in range(SEQUENCE_LENGTH):
                 print(
-                    f"{test_input_decoded[l]: <{padding}}{test_output_decoded[l]: <{padding}}{test_labels_decoded[l]: <{padding}}", file=g)
+                    f"{prediction_input_decoded[l]: <{padding}}{prediction_output_decoded[l]: <{padding}}", file=g)
 
 
-with open('war_and_peace.txt', 'r') as f:
-    data = f.read()
-
-# Create a TextBlob object from the input data
-data = TextBlob(data)
-# Get a part-of-speech tag describing each word and strip the text of all additional characters like commas, dots, hyphens, etc.
-words_pos_tags = data.tags
-# Obtain words from tuples of (word, pos_tag) in order to encode them
-words = [word for word, tag in words_pos_tags]
-# Remove duplicates
-unique_words = list(set(words))
-
-words_pos_tags_indexed = []
-
-# Turn POS tags into numbers in the following way:
-# - noun --> 0
-# - verb --> 1
-# - adjective --> 2
-# - adverb --> 3
-# - other --> 4
-for word, tag in words_pos_tags:
-    if (tag[0] == 'N'):
-        words_pos_tags_indexed.append((word, 0))
-    elif (tag[0] == 'V'):
-        words_pos_tags_indexed.append((word, 1))
-    elif (tag[0] == 'J'):
-        words_pos_tags_indexed.append((word, 2))
-    elif (tag[0] == 'R'):
-        words_pos_tags_indexed.append((word, 3))
-    else:
-        words_pos_tags_indexed.append((word, 4))
-
-# A dictionary to turn an index into a word
-ix_to_word = {ix: word for ix, word in enumerate(unique_words)}
-# A dictionary to turn a word into an index
-word_to_ix = {word: ix for ix, word in enumerate(unique_words)}
-# A dictionary to turn an index into a part of speech
-ix_to_tag = {0: 'NOUN', 1: 'VERB', 2: 'ADJECTIVE', 3: 'ADVERB', 4: 'OTHER'}
-
-# Number of unique words
-VOCABULARY_SIZE = len(unique_words)
-# Number of labels
-NUM_OF_LABELS = len(ix_to_tag)
-# Length of one sequence of words input to the network
+# The length of a single sequence of words passed to LSTM cells
 SEQUENCE_LENGTH = 10
-# Number of sequences that can be created from the input data
-NUM_OF_SEQUENCES = len(words) // SEQUENCE_LENGTH
-# How much of the data is to be used for training
-TRAIN_DATA_FRACTION = 6 / 8
-# Set the number of training and test sequences
-TRAIN_DATA_NUM = int(TRAIN_DATA_FRACTION * NUM_OF_SEQUENCES)
-TEST_DATA_NUM = NUM_OF_SEQUENCES - TRAIN_DATA_NUM
-
-OUTPUT_FILE = "results.txt"
-
-# Flag indicating if LSTMs are only in the forward pass mode
-ONLY_FW_PASS = False
 
 # Size of tensor containing hidden state of the cell, does not have any correlation with input, output or target
 LSTM_SIZE = 128
@@ -275,70 +302,16 @@ FIRST_LAYER_UNITS = 64
 # Number of epochs to be spend on learning
 NUM_EPOCHS = 1
 
-# Data preparation, one-hot encoded words and labels
-X = np.zeros((NUM_OF_SEQUENCES, SEQUENCE_LENGTH, VOCABULARY_SIZE))
-y = np.zeros((NUM_OF_SEQUENCES, SEQUENCE_LENGTH, NUM_OF_LABELS))
-for i in range(0, NUM_OF_SEQUENCES):
-    X_sequence = words_pos_tags_indexed[i *
-                                        SEQUENCE_LENGTH:(i+1)*SEQUENCE_LENGTH]
-    X_sequence_ix = [word_to_ix[word] for word, tag in X_sequence]
-    input_sequence = np.zeros((SEQUENCE_LENGTH, VOCABULARY_SIZE))
-    for j in range(SEQUENCE_LENGTH):
-        input_sequence[j][X_sequence_ix[j]] = 1.
-    X[i] = input_sequence
+print("""
+    Welcome to the Text Annotator! Remember to follow the right sequence of instructions (load -> train -> predict). Type the following commands to get started:
 
-    target_sequence = np.zeros((SEQUENCE_LENGTH, NUM_OF_LABELS))
-    for j in range(SEQUENCE_LENGTH):
-        target_sequence[j][X_sequence[j][1]] = 1.
-    y[i] = target_sequence
-
-# Divide the dataset into training and testing parts
-trainX = X[:TRAIN_DATA_NUM]
-trainY = y[:TRAIN_DATA_NUM]
-
-testX = X[TRAIN_DATA_NUM:]
-testY = y[TRAIN_DATA_NUM:]
-
-# print("\nThe chosen architecture:\n\n"
-#       f"Size of LSTM hidden vectors: {LSTM_SIZE}")
-
-# if ONLY_FW_PASS:
-#     print(f"Number of forward LSTMs in the first layer: {LSTM_ONLY_FW_NUMBER_FIRST_LAYER}\n"
-#           f"Number of forward LSTMs in the second layer: {LSTM_ONLY_FW_NUMBER_SECOND_LAYER}", end='')
-# else:
-#     print(f"Number of forward LSTMs in the first layer: {LSTM_FW_NUMBER_FIRST_LAYER}\n"
-#           f"Number of backward LSTMs in the first layer: {LSTM_BW_NUMBER_FIRST_LAYER}\n"
-#           f"Number of forward LSTMs in the second layer: {LSTM_FW_NUMBER_SECOND_LAYER}\n"
-#           f"Number of backward LSTMs in the second layer: {LSTM_BW_NUMBER_SECOND_LAYER}", end='')
-
-# print(
-#     f"\nNumber of units in the output of the first layer: {FIRST_LAYER_UNITS}\n")
-
-# print("#" * 72, "\n")
-
-# print("Finished loading and preparing the data!\n\n"
-#       f"Sequence length: {SEQUENCE_LENGTH}\n"
-#       f"Number of labels: {NUM_OF_LABELS}\n"
-#       f"Number of unique words: {VOCABULARY_SIZE}\n\n"
-#       f"Number of sequences: {NUM_OF_SEQUENCES}\n"
-#       f"Number of training sequences: {TRAIN_DATA_NUM}\n"
-#       f"Number of test sequences: {TEST_DATA_NUM}\n\n"
-#       f"Number of training epochs: {NUM_EPOCHS}\n")
-
-# print("#" * 72)
-
-while True:
-    print("""
-    Welcome to the Text Annotator! Remember to follow the right sequence of instructions (load -> build -> train -> test or predict). Type the following commands to get started:
-
-    load    ---> load the training and testing data
-    build   ---> build the model
+    load    ---> load the training and testing data and build the model
     train   ---> train the model
-    test    ---> test the model
     predict ---> generate predictions and write them to a file
     quit    ---> quit the program
     """)
 
+while True:
     ans = input("What would you like to do? ---> ")
 
     try:
@@ -347,23 +320,30 @@ while True:
 
             filename = input("Type the name of the file with text inside: ")
 
-        elif ans == "build":
+            print("\nStarting loading and preparing the data...")
+
+            vocabulary_size, num_of_labels, ix_to_word, word_to_ix, ix_to_tag, trainX, trainY, testX, testY, train_data_num, test_data_num, original_dataset = load_and_parse_data(
+                filename, SEQUENCE_LENGTH, 7/8)
+
+            print("\nFinished loading and preparing the data!")
 
             print("\nStarting building the model...")
+
+            tf.reset_default_graph()
 
             # A placeholder for input, i.e. a matrix of shape SEQUENCE_LENGTH x VOCABULARY_SIZE
             # , the shape of the placeholder is (None, SEQUENCE_LENGTH, VOCABULARY_SIZE
             # ), because None is reserved for batch size
             input_data = tf.placeholder(tf.float32, shape=(
-                None, SEQUENCE_LENGTH, VOCABULARY_SIZE
+                None, SEQUENCE_LENGTH, vocabulary_size
             ), name='input')
 
             # A placeholder for targets, i.e. a matrix of shape SEQUENCE_LENGTH x NUM_OF_LABELS, the shape of the placeholder is (SEQUENCE_LENGTH, NUM_OF_LABELS)
             targets = tf.placeholder(tf.float32, shape=(
-                SEQUENCE_LENGTH, NUM_OF_LABELS), name='targets')
+                SEQUENCE_LENGTH, num_of_labels), name='targets')
 
             logits = build_text_annotation_model(False, LSTM_SIZE, LSTM_FW_NUMBER_FIRST_LAYER, LSTM_BW_NUMBER_FIRST_LAYER,
-                                                 LSTM_FW_NUMBER_SECOND_LAYER, LSTM_BW_NUMBER_SECOND_LAYER, FIRST_LAYER_UNITS, NUM_OF_LABELS, input_data)
+                                                 LSTM_FW_NUMBER_SECOND_LAYER, LSTM_BW_NUMBER_SECOND_LAYER, FIRST_LAYER_UNITS, num_of_labels, input_data)
 
             print("\nFinished building the model!")
 
@@ -371,24 +351,21 @@ while True:
 
             print("\nStarting training phase...\n")
 
-            sess = train_text_annotation_model(logits, targets, NUM_EPOCHS)
+            sess = train_text_annotation_model(
+                logits, targets, trainX, trainY, testX, testY, train_data_num, test_data_num, NUM_EPOCHS)
 
             print("\nFinished training phase!\n")
 
-        elif ans == "test":
-
-            print("\nStarting testing phase...\n")
-
-            test_text_annotation_model(logits, targets, sess)
-
-            print("\nFinished testing phase!\n")
-
         elif ans == "predict":
 
-            predict_filename = input(
+            input_file = input(
+                "Type the name of the file with input data: ")
+
+            output_file = input(
                 "Type the name of the file to write predictions to: ")
 
-            predict_text_annotation_model(logits, predict_filename, sess)
+            predict_text_annotation_model(
+                logits, ix_to_word, word_to_ix, SEQUENCE_LENGTH, num_of_labels, vocabulary_size, original_dataset, input_file, output_file, sess)
 
             print("\nFinished printing the words and the labels!\n")
 
@@ -398,11 +375,8 @@ while True:
 
     except:
         print(
-            "\nSorry, probably you did not follow the right order of instructions :( Please, try again!\n")
+            "\nSorry, something went wrong :( Please, try again!\n")
 
     print("\n", "#" * 72, "\n\n")
-
-    # time.sleep(3)
-    # os.system('cls' if os.name == 'nt' else 'clear')
 
 print("\nThank you for using me! Have a jolly good day, goodbye :)\n")
